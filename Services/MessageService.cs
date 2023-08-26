ec3d42bd01d77f94df438ae8251b9ad3fff6f24a
Esquetta <esquetta@gmail.com>
2023-08-26 16:13:53 +0300

    Add project files.

---
+++ Services/MessageService.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Core;
+using static GrpcServiceClientStreaming.Message;
+
+namespace GrpcServiceClientStreaming.Services
+{
+    public class MessageService:MessageBase
+    {
+
+        public override async Task<MessageResponse> GetMessage(IAsyncStreamReader<MessageRequest> requestStream, ServerCallContext context)
+        {
+            await Task.Run(async () => {
+
+                while (await requestStream.MoveNext())
+                {
+                    Console.WriteLine($"Mesaj alınmıştır.");
+                    Console.WriteLine("Gelen mesaj : ");
+                    Console.WriteLine(requestStream.Current.Message);
+                }
+            
+            });
+
+            return new MessageResponse { Message = "İstek alındı ve işlendi..." };
+        }
+    }
+}
